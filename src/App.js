15ae76b058950673cc0f7430fde33e9885bd425c
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-08-06 21:21:08 -0300

    using props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import NoteList from "./components/NoteList";
 import RegistrationForm from "./components/RegistrationForm";
-import './assets/App.css';
-import './assets/index.css';
+import "./assets/App.css";
+import "./assets/index.css";
 
 class App extends Component {
+  createNote(title, text) {
+    console.log(`new card created ${title}, ${text}`);
+  }
   render() {
     return (
-      <section className='content'>
-        <RegistrationForm></RegistrationForm>
+      <section className="content">
+        <RegistrationForm createNote={this.createNote}></RegistrationForm>
         <NoteList></NoteList>
       </section>
     );
-  };
-};
+  }
+}
 
 export default App;
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -2,31 +2,43 @@ import React, { Component } from "react";
 import "./style.css";
 
 class RegistrationForm extends Component {
-
-  constructor() {
-    super();
-    this.title = '';
+  constructor(props) {
+    super(props);
+    this.title = "";
+    this.text = "";
   }
 
-  handlerChangeTitle(event) {
+  _handlerChangeTitle(event) {
     this.title = event.target.value;
-    console.log(this.title);
+  }
+
+  _handlerChangeText(event) {
+    this.text = event.target.value;
+  }
+
+  createNote(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.props.createNote(this.title, this.text);
   }
 
   render() {
     return (
-      <form className="registration-form">
+      <form className="registration-form"
+        onSubmit={this.createNote.bind(this)}
+      >
         <input
           type="text"
           placeholder="Title"
           className="registration-form_input"
-          onChange={this.handlerChangeTitle.bind(this)}
+          onChange={this._handlerChangeTitle.bind(this)}
         />
         <textarea
           rows={15}
           className="registration-form_"
           placeholder="Write your note..."
           className="registration-form_input"
+          onChange={this._handlerChangeText.bind(this)}
         />
         <button className="registration-form_input registration-form_submit">
           Create note

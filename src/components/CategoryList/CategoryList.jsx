d640da1baf5833e74c821d28c9510eaa8b262a23
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-08-11 21:10:46 -0300

    adding stylesheet

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,29 +1,35 @@
 import React, { Component } from "react";
+import './style.css';
 
 class CategoryList extends Component {
   _handleEventInput(event) {
     if (event.key == "Enter") {
       let categoryValue = event.target.value;
       this.props.addCategory(categoryValue);
-    };
-  };
+    }
+  }
 
   render() {
     return (
-      <section>
-        <ul>
+      <section className="list-category">
+        <ul className="list-category_list">
           {this.props.categoryList.map((category, index) => {
-            return <li key={index}>{category}</li>;
+            return (
+              <li className="list-category_item" key={index}>
+                {category}
+              </li>
+            );
           })}
         </ul>
         <input
           type="text"
           placeholder="Adicionar Categoria"
+          className="list-category_input"
           onKeyUp={this._handleEventInput.bind(this)}
         ></input>
       </section>
     );
-  };
-};
+  }
+}
 
 export default CategoryList;

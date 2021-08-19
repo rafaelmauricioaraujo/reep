896434edfb88d1a1cd42c68abfc26bed33c6bb25
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-08-18 22:46:06 -0300

    using material ui design

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 function UserForm() {
   return (
     <form>
+      <label>First Name</label>
       <input type="text" />
+
+      <label>Last Name</label>
+      <input type="text" />
+
+      <label>ID Code</label>
+      <input type="text" />
+
+      <label>Promotions</label>
+      <input type="checkbox" />
+
+      <label>Newsletter</label>
+      <input type="checkbox" />
+
+      <Button variant="contained" color="primary" type="submit">
+        Register
+      </Button>
     </form>
   );
 }

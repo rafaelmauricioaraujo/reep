d784bfd668ca58a1c464a1eb3661732bfbd84e89
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-08-19 23:18:49 -0300

    using hooks in user form

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function UserForm() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(firstName);
+        console.log(secondName);
+      }}
+    >
       <TextField
         id="standard-basic"
         label="First Name"
         margin="normal"
         variant="outlined"
         fullWidth
+        value={firstName}
+        onChange={(event) => {
+          let temp = event.target.value;
+          if (temp >= 3) {
+            temp = temp.substr(0, 3);
+          }
+          setFirstName(temp);
+        }}
       />
 
       <TextField
@@ -18,6 +35,10 @@ function UserForm() {
         margin="normal"
         variant="outlined"
         fullWidth
+        value={secondName}
+        onChange={(event) => {
+          setSecondName(event.target.value);
+        }}
       />
 
       <TextField

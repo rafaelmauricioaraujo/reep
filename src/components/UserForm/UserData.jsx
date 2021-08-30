e0c2b0a032242f5ca2f416e250a6a1e4f28c59c7
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-08-30 20:57:05 -0300

    adding field to form of register

---
+++ src/components/UserForm/UserData.jsx
@@ -0,0 +1,30 @@
+import { TextField, Button } from "@material-ui/core";
+import React from "react";
+
+function UserData() {
+  return (
+    <form>
+      <TextField
+        id="email"
+        label="email"
+        type="email"
+        margin="normal"
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        id="password"
+        label="password"
+        type="password"
+        margin="normal"
+        variant="outlined"
+        fullWidth
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Register
+      </Button>
+    </form>
+  );
+}
+
+export default UserData;
--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import PersonalData from "./PersonalData";
+import UserData from "./UserData";
 
 function UserForm({ onSubmit, validEmail }) {
   return (
-    <PersonalData />
+    <>
+      <PersonalData onSubmit={onSubmit} validEmail={validEmail} />
+      <UserData />
+    </>
   );
 }
 

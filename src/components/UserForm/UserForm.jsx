66ee0c210d15e144c4a4d4faa6ab9ccd3ef789a8
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-08-31 22:54:02 -0300

    using switch case to show one user form

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
 import PersonalData from "./PersonalData";
 import ShipmentData from "./ShipmentData";
 import UserData from "./UserData";
 
 function UserForm({ onSubmit, validEmail }) {
-  return (
-    <>
-      <PersonalData onSubmit={onSubmit} validEmail={validEmail} />
-      <UserData />
-      <ShipmentData />
-    </>
-  );
+  const [currentState, setCurrentState] = useState(0);
+  
+  function currentForm(currentState) {
+    switch (currentState) {
+      case 0:
+        return <PersonalData onSubmit={onSubmit} validEmail={validEmail} />;
+      case 1:
+        return <UserData />;
+      case 2:
+        return <ShipmentData />;
+      default:
+        return <Typography>Error. Form not recognize</Typography>;
+    }
+  }
+
+  return <>{currentForm(currentState)}</>;
 }
+
 
 export default UserForm;

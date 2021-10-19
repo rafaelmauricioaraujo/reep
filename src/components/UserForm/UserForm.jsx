40ccd8c6858a1eff94456a80b9943b9b13737b05
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-10-19 09:43:01 -0300

    using useEffect hook to sincronyze data in user form

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PersonalData from "./PersonalData";
 import ShipmentData from "./ShipmentData";
 import UserData from "./UserData";
 
 function UserForm({ onSubmit, validEmail }) {
   const [currentStep, setCurrentStep] = useState(0);
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    console.log(userData);
+  });
 
   const forms = [
-    <UserData onSubmit={next} />,
-    <PersonalData onSubmit={next} validEmail={validEmail} />,
+    <UserData onSubmit={getUserData} />,
+    <PersonalData onSubmit={getUserData} validEmail={validEmail} />,
     <ShipmentData onSubmit={onSubmit} />,
   ];
+
+  function getUserData(data) {
+    setUserData({ ...userData, ...data });
+    next();
+  }
 
   function next() {
     setCurrentStep(currentStep + 1);
